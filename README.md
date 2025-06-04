ce88d761538352197eadb85dc306c6bad4192d32
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-06-04 10:30:09 +0530

    done changes only left 2

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 todo :
 
-1. fix audio time and h1 - style in html
-2. set color of bar and height
-3. get a good audio
-4. remove /public from ignore
-5. add a functional loader for audio
+1. fix audio time and h1 - style in html -done
+2. set color of bar and height -done 
+3. get a good audio -done
+4. remove /public from ignore -done
+5. add a functional loader for audio 
 x. clean code
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,21 +1,23 @@
 varying vec2 vUv;
     uniform float uAmplitude;
     void main() {
-      vec3 colorStart = vec3(0.0, 0.0, 0.0); // #FF7722 (normalized)
-      // vec3 colorEnd = vec3(1.0,1.0, 1.0);  
-      vec3 colorEnd = vec3(0.6);
-      vec3 baseColor = mix(colorStart, colorEnd, uAmplitude);
+      // vec3 colorStart = vec3(0.0, 0.0, 0.0); // #FF7722 (normalized)
+      // // vec3 colorEnd = vec3(1.0,1.0, 1.0);  
+      // vec3 colorEnd = vec3(0.6);
+      // vec3 baseColor = mix(colorStart, colorEnd, uAmplitude);
 
-      // RGB distortion based on amplitude
-      float distortionAmount = uAmplitude * 0.02; // Adjust distortion intensity
-      vec2 uvR = vUv + vec2(distortionAmount, 0.0);
-      vec2 uvG = vUv;
-      vec2 uvB = vUv - vec2(distortionAmount, 0.0);
+      // // RGB distortion based on amplitude
+      // float distortionAmount = uAmplitude * 0.02; // Adjust distortion intensity
+      // vec2 uvR = vUv + vec2(distortionAmount, 0.0);
+      // vec2 uvG = vUv;
+      // vec2 uvB = vUv - vec2(distortionAmount, 0.0);
 
-      // Sample color for each channel with offset UVs
-      float r = mix(colorStart.r, colorEnd.r, uAmplitude * uvR.y);
-      float g = mix(colorStart.g, colorEnd.g, uAmplitude * uvG.y);
-      float b = mix(colorStart.b, colorEnd.b, uAmplitude * uvB.y);
+      // // Sample color for each channel with offset UVs
+      // float r = mix(colorStart.r, colorEnd.r, uAmplitude * uvR.y);
+      // float g = mix(colorStart.g, colorEnd.g, uAmplitude * uvG.y);
+      // float b = mix(colorStart.b, colorEnd.b, uAmplitude * uvB.y);
 
-      gl_FragColor = vec4(r, g, b, 1.0);
+      // gl_FragColor = vec4(r, g, b, 1.0);
+      vec3 color = vec3(0.6);
+      gl_FragColor = vec4(color, 1.0);
     }
